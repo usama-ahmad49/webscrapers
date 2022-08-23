9ae7b87cf92046f2213406edb6a8b6a8f71e09bd
usama-ahmad49 <usamaahmed2222@gmail.com>
2022-08-23 17:04:08 +0500

    facebook and outlook files updated

--- scraping/scraping/spiders/email_reader.py
+++ scraping/scraping/spiders/email_reader.py
@@ -7,7 +7,26 @@ inbox = mapi.GetDefaultFolder(6)
 
 def create_reply(email:object):
     reply = email.Reply()
-    body = "Body Text"
+    body = '''Thank you for responding to our ad. The landlord has already received over 30 responses and will probably pick a tenant from those applications. I suggest that you register on the website https://www.homefinders.rentals 
+
+and you will start getting notified with rentals BEFORE we post them here, so you won't miss out on any more great listings!  It is FREE to Register !!
+
+With over 1,100 properties, covering 24 cities across BC, HomeFinders has you covered. You can;
+-Search our database (for Free)
+-Register (for Free) and be notified when a new rental hits the market that matches what you are looking for.
+- Hire us to search through 13 different rental websites every day (so you don't have to).
+- Hire us to tip you off on rentals BEFORE they hit the market.
+
+Imagine waking up to a NEW list of rentals every day until you find a place!
+
+Check out this information Video https://youtu.be/7x1x0skLRWs
+
+Proud Member of the Better Business Bureau with an A+ rating for the last 17 years.
+https://www.bbb.org/.../renta.../homefinders-0037-2417284...
+
+
+
+'''
     reply.HTMLBody = body + reply.HTMLBody
     reply.Send()
 
--- scraping/scraping/spiders/facebook_auto_reply.py
+++ scraping/scraping/spiders/facebook_auto_reply.py
@@ -8,6 +8,27 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
 if __name__ == '__main__':
+    AutomatedMessage = '''Thank you for responding to our ad. The landlord has already received over 30 responses and will probably pick a tenant from those applications. I suggest that you register on the website https://www.homefinders.rentals 
+
+and you will start getting notified with rentals BEFORE we post them here, so you won't miss out on any more great listings!  It is FREE to Register !!
+
+With over 1,100 properties, covering 24 cities across BC, HomeFinders has you covered. You can;
+-Search our database (for Free)
+-Register (for Free) and be notified when a new rental hits the market that matches what you are looking for.
+- Hire us to search through 13 different rental websites every day (so you don't have to).
+- Hire us to tip you off on rentals BEFORE they hit the market.
+
+Imagine waking up to a NEW list of rentals every day until you find a place!
+
+Check out this information Video https://youtu.be/7x1x0skLRWs
+
+Proud Member of the Better Business Bureau with an A+ rating for the last 17 years.
+https://www.bbb.org/.../renta.../homefinders-0037-2417284...
+
+
+
+'''
+
     username = '**********'
     password = '******'
 
@@ -35,7 +56,7 @@ if __name__ == '__main__':
         time.sleep(1)
         driver.find_element(By.CSS_SELECTOR,'div[role="textbox"]').clear()
         time.sleep(1)
-        driver.find_element(By.CSS_SELECTOR,'div[role="textbox"]').send_keys('Automated message')
+        driver.find_element(By.CSS_SELECTOR,'div[role="textbox"]').send_keys(AutomatedMessage)
         time.sleep(1)
         driver.find_element(By.CSS_SELECTOR,'div[role="textbox"]').send_keys(Keys.ENTER)
         time.sleep(1)
