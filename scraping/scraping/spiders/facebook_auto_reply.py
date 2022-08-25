6c293ca9c31a2c46852ed9a278a4a397092f9e97
usama-ahmad49 <usamaahmed2222@gmail.com>
2022-08-25 19:05:30 +0500

    facebook auto reply script modified for multiple accounts

--- scraping/scraping/spiders/facebook_auto_reply.py
+++ scraping/scraping/spiders/facebook_auto_reply.py
@@ -27,47 +27,53 @@ if __name__ == '__main__':
         print('error opening file')
         exit()
 
-    username = 'f.nobel301@gmail.com'
-    password = 'Kelowna1!'
+    with open("Facebook_Accounts_file.txt",'r', encoding='utf-8') as FAF:
+        DATA = FAF.read().split('\n')
 
-    options = Options()
-    options.add_argument('--disable-notifications')
-    driver = webdriver.Chrome(options=options)
-    driver.maximize_window()
-    driver.get('https://www.facebook.com/')
-    driver.find_element(By.ID, 'email').send_keys(username)
-    driver.find_element(By.ID, 'pass').send_keys(password)
-    driver.find_element(By.CSS_SELECTOR, "button[name='login']").click()
-    WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'input[type="search"]')))
-    time.sleep(3)
+    for data in DATA:
+        if data == '':
+            continue
+        username = data.split('>')[0]
+        password = data.split('>')[1]
 
-    driver.get('https://www.facebook.com/marketplace/inbox')
-    time.sleep(3)
-    WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'div[aria-label="Collection of Marketplace items"]')))
-    driver.find_element(By.CSS_SELECTOR,'div[aria-label="Collection of Marketplace items"]')
-    time.sleep(3)
-    for messages in driver.find_elements(By.CSS_SELECTOR,'div[aria-label="Collection of Marketplace items"] div[data-visualcompletion="ignore-dynamic"]')[1:]:
-        messages.click()
-        time.sleep(3)
-        driver.find_element(By.CSS_SELECTOR,'div[role="textbox"]').click()
-        time.sleep(1)
-        driver.find_element(By.CSS_SELECTOR,'div[role="textbox"]').clear()
-        time.sleep(1)
-        driver.find_element(By.CSS_SELECTOR,'div[role="textbox"]').send_keys(AutomatedMessage.replace('\n','\r'))
-        time.sleep(1)
-        driver.find_element(By.CSS_SELECTOR,'div[role="textbox"]').send_keys(Keys.ENTER)
-        time.sleep(1)
-
-
-
-        driver.find_element(By.CSS_SELECTOR,'div[aria-label="Close chat"]').click()
+        options = Options()
+        options.add_argument('--disable-notifications')
+        driver = webdriver.Chrome(options=options)
+        driver.maximize_window()
+        driver.get('https://www.facebook.com/')
+        driver.find_element(By.ID, 'email').send_keys(username)
+        driver.find_element(By.ID, 'pass').send_keys(password)
+        driver.find_element(By.CSS_SELECTOR, "button[name='login']").click()
+        WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'input[type="search"]')))
         time.sleep(3)
 
-    driver.find_element(By.CSS_SELECTOR, 'div[aria-label="Your profile"]').click()
-    time.sleep(1)
-    driver.find_element(By.CSS_SELECTOR, 'div[data-visualcompletion="ignore-dynamic"][role="listitem"][data-nocookies="true"]').click()
+        driver.get('https://www.facebook.com/marketplace/inbox')
+        time.sleep(3)
+        WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'div[aria-label="Collection of Marketplace items"]')))
+        driver.find_element(By.CSS_SELECTOR,'div[aria-label="Collection of Marketplace items"]')
+        time.sleep(3)
+        for messages in driver.find_elements(By.CSS_SELECTOR,'div[aria-label="Collection of Marketplace items"] div[data-visualcompletion="ignore-dynamic"]')[1:]:
+            messages.click()
+            time.sleep(3)
+            driver.find_element(By.CSS_SELECTOR,'div[role="textbox"]').click()
+            time.sleep(1)
+            driver.find_element(By.CSS_SELECTOR,'div[role="textbox"]').clear()
+            time.sleep(1)
+            driver.find_element(By.CSS_SELECTOR,'div[role="textbox"]').send_keys(AutomatedMessage.replace('\n','\r'))
+            time.sleep(1)
+            driver.find_element(By.CSS_SELECTOR,'div[role="textbox"]').send_keys(Keys.ENTER)
+            time.sleep(1)
 
-    driver.quit()
+
+
+            driver.find_element(By.CSS_SELECTOR,'div[aria-label="Close chat"]').click()
+            time.sleep(3)
+
+        driver.find_element(By.CSS_SELECTOR, 'div[aria-label="Your profile"]').click()
+        time.sleep(1)
+        driver.find_element(By.CSS_SELECTOR, 'div[data-visualcompletion="ignore-dynamic"][role="listitem"][data-nocookies="true"]').click()
+
+        driver.quit()
 
 
 
