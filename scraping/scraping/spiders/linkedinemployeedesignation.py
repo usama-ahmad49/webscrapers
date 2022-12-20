f14202c1e18a4e11fc1a4d097ca673a2a83b90f3
Usama Ahmad <usamaahmed2222@gmail.com>
2022-12-20 20:25:52 +0500

    linkedinemployeedesignation.py

--- scraping/scraping/spiders/linkedinemployeedesignation.py
+++ scraping/scraping/spiders/linkedinemployeedesignation.py
@@ -1,25 +1,17 @@
 import time
-
-import requests
+import pandas as pd
 import scrapy
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 import urllib.request
 import os
+import re
 import csv
-cwd = os.getcwd()
 
 
-# value = open('proxies.txt','r')
-# proxy = value.readlines()[0]
-#
-# certificate = os.path.join(cwd, 'zyte-smartproxy-ca.crt')
-# proxy_auth = proxy
-# proxies={
-#         "http": f"http://{proxy_auth}@proxy.crawlera.com:8011/",
-#         "https": f"http://{proxy_auth}@proxy.crawlera.com:8011/",
-#     }
+
 def set_http_proxy(proxy):
         if proxy == None: # Use system default setting
             proxy_support = urllib.request.ProxyHandler()
@@ -50,20 +42,6 @@ def makebrowserandsignin():
 
     return driver
 
-# def getgooglelinks(filedesignations):
-#     for des in filedesignations:
-#         if des != '':
-#             url = f'https://www.google.com/search?q=site:linkedin.com {companyname} intitle"{des}"&sxsrf=ALiCzsZ0Q9wAg43iYoRf35wNpTjxpCJtGw:1666909552823&ei=cAVbY6XnMf7cptQPzu2pgAk&ved=0ahUKEwil35L-uYH7AhV-rokEHc52CpAQ4dUDCA8&uact=5&oq=site:linkedin.com {companyname} intitle"{des}"&gs_lcp=Cgdnd3Mtd2l6EANKBAhBGAFKBAhGGABQ6RdY6RdghCVoAnAAeACAAZ0BiAGdAZIBAzAuMZgBAKABAqABAcABAQ&sclient=gws-wiz&num=100'
-#
-#             response = requests.get(url, proxies=proxies, verify=certificate)
-#             googleselector = scrapy.Selector(text=response.text)
-#             googlelinklist = googleselector.css('.kvH3mc.BToiNc.UK95Uc a::attr(href)').extract()
-#             with open('googlelinklist.txt','a+') as gll:
-#                 for gl in googlelinklist:
-#                     if gl != '#':
-#                         gll.write(gl+'\n')
-
-
 def parse_profiles_page(driver):
     Selector = scrapy.Selector(text=driver.page_source)
     for sel in Selector.css('.reusable-search__result-container'):
@@ -78,65 +56,129 @@ def parse_profiles_page(driver):
 
 
 if __name__ == '__main__':
+    csvheaders = ['name', 'country', 'location', 'job','public', 'profile link', 'followers','Email']
+    csvoutfile = open('linkedinoutputcsv.csv','a+',encoding='utf-8', newline='')
+    csvwriter = csv.DictWriter(csvoutfile,fieldnames = csvheaders)
+    file_exists = os.path.isfile('linkedinoutputcsv.csv')
+    if not file_exists:
+        csvwriter.writeheader()
     uniquedesignations = []
     csvread = open('linkedininput.csv', mode='r')
+    csvreadfield = ['Keyword', 'Location', 'PagesToScrape', 'LastPageScraped', 'Talk_About']
     csv_reader = csv.DictReader(csvread)
 
-    # fileinput = open('inputlinkedin.csv','r', encoding='utf-8')
-    # companynames = fileinput.read().split('\n')
+
     driver = makebrowserandsignin()
+    df = pd.read_csv("linkedininput.csv")
     line_count = 0
     for row in csv_reader:
-        # if line_count == 0:
-        #     print(f'Column names are {", ".join(row)}')
-        #     line_count += 1
-        #     continue
         driver.get('https://www.linkedin.com/search/results/people/')
+        driver.find_element(By.CSS_SELECTOR,'input.search-global-typeahead__input').send_keys(row['Keyword'])
+        time.sleep(.5)
+        driver.find_element(By.CSS_SELECTOR,'input.search-global-typeahead__input').send_keys(Keys.RETURN)
+        time.sleep(3)
         driver.find_element(By.CSS_SELECTOR,'button[aria-label="Show all filters. Clicking this button displays all available filter options."]').click()
         time.sleep(1)
-        # driver.find_element(By.CSS_SELECTOR,'input[aria-label="Add a company"]').click()
-        # driver.find_element(By.CSS_SELECTOR,'input[aria-label="Add a company"]').send_keys(row['Keyword'])
-        # time.sleep(2)
-        # driver.find_element(By.CSS_SELECTOR,'.basic-typeahead__selectable').click()
-        # time.sleep(2)
-        # driver.find_element(By.CSS_SELECTOR,'#hoverable-outlet-current-company-filter-value button[aria-label="Apply current filter to show results"]').click()
-        time.sleep(2)
-
         for section in driver.find_elements(By.CSS_SELECTOR, '.search-reusables__secondary-filters-filter'):
             if 'Location' in section.find_element(By.CSS_SELECTOR, 'h3').text:
-                loc = [v for v in driver.find_elements(By.CSS_SELECTOR, '.search-reusables__filter-value-item') if
+                loc = [v for v in section.find_elements(By.CSS_SELECTOR, '.search-reusables__filter-value-item')[:-1] if
                        row['Location'] in v.find_element(By.CSS_SELECTOR, 'label p span').text]
                 if len(loc) < 1:
-                    driver.find_elements(By.CSS_SELECTOR, '.search-reusables__filter-value-item')[-1].find_element(
+                    section.find_elements(By.CSS_SELECTOR, '.search-reusables__filter-value-item')[-1].find_element(
                         By.CSS_SELECTOR, 'button').click()
                     time.sleep(2)
-                    driver.find_element(By.CSS_SELECTOR, 'input[aria-label="Add a location"]').send_keys(
+                    section.find_element(By.CSS_SELECTOR, 'input[aria-label="Add a location"]').send_keys(
                         row['Location'])
+                    time.sleep(2)
+                    section.find_element(By.CSS_SELECTOR,
+                                         '.basic-typeahead__triggered-content div.basic-typeahead__selectable').click()
+                    time.sleep(1)
+
+                else:
+                    loc[0].find_element(By.CSS_SELECTOR,'label').click()
+                    time.sleep(1)
+
+            if 'Talks about' in section.find_element(By.CSS_SELECTOR, 'h3').text:
+                tk_ab = [v for v in section.find_elements(By.CSS_SELECTOR, '.search-reusables__filter-value-item')[:-1] if
+                         ('#'+row['Talk_About']) in v.find_element(By.CSS_SELECTOR, 'label p span').text]
+                if len(tk_ab) < 1:
+                    section.find_elements(By.CSS_SELECTOR, '.search-reusables__filter-value-item')[-1].find_element(
+                        By.CSS_SELECTOR, 'button').click()
+                    time.sleep(2)
+                    section.find_element(By.CSS_SELECTOR, 'input[aria-label="Search a topic"]').send_keys(
+                        row['Talk_About'])
+                    time.sleep(2)
+                    section.find_element(By.CSS_SELECTOR,'.basic-typeahead__triggered-content div.basic-typeahead__selectable').click()
+                    time.sleep(1)
+                else:
+                    tk_ab[0].find_element(By.CSS_SELECTOR,'label').click()
+                    time.sleep(1)
+
+        time.sleep(3)
+        driver.find_element(By.CSS_SELECTOR,'button[aria-label="Apply current filters to show results"]').click()
+
+        time.sleep(3)
+        LastPageScraped = int(row['LastPageScraped'])
+        TotalPages = int(row['PagesToScrape'])
+        driver.get(driver.current_url+f'&page={LastPageScraped}')
+        for i in range(1,TotalPages):
+            for member in driver.find_elements(By.CSS_SELECTOR,'.reusable-search__result-container'):
+                if 'LinkedIn Member' in member.find_element(By.CSS_SELECTOR,'span.entity-result__title-text a').text:
+                    continue
+                    # item = dict()
+                    # item['name'] = member.find_element(By.CSS_SELECTOR,'span.entity-result__title-text a').text
+                    # item['country'] = row['Location']
+                    # item['location'] = member.find_element(By.CSS_SELECTOR,'.entity-result__secondary-subtitle').text.strip()
+                    # item['job'] = member.find_element(By.CSS_SELECTOR,'.entity-result__primary-subtitle').text.strip()
+                    # item['public'] = 'No'
+                    # item['profile link'] = 'No'
+                    # item['followers'] = 'Unknown'
+                    # csvwriter.writerow(item)
+                    # csvoutfile.flush()
+
+
+                else:
+                    item = dict()
+                    item['name'] = member.find_element(By.CSS_SELECTOR, 'span.entity-result__title-text a').text.split('\n')[0].strip()
+                    item['country'] = row['Location']
+                    item['location'] = member.find_element(By.CSS_SELECTOR,
+                                                           '.entity-result__secondary-subtitle').text.strip()
+                    item['job'] = member.find_element(By.CSS_SELECTOR, '.entity-result__primary-subtitle').text.strip()
+                    item['public'] = 'Yes'
+                    item['profile link'] = member.find_element(By.CSS_SELECTOR, 'span.entity-result__title-text a').get_attribute('href').split('?')[0]
+                    try:
+                        item['followers'] = member.find_element(By.CSS_SELECTOR, '.entity-result__simple-insight-text-container span').text.strip().split('followers')[0].strip()
+                    except:
+                        pass
+                    driver.execute_script("window.open('');")
                     time.sleep(.5)
-                    driver.find_element(By.CSS_SELECTOR, 'input[aria-label="Add a location"]')
+                    driver.switch_to.window(driver.window_handles[-1])
+                    time.sleep(.5)
+                    driver.get(item['profile link']+'/overlay/contact-info/')
 
-        totalresults = int(driver.find_element(By.CSS_SELECTOR,'.search-results-container h2').text.split(' ')[0].replace(',',''))
-        pages = totalresults/10
-        pages = int(pages)+1
-        if pages>101:
-            pages = 101
-        link = driver.current_url
-        for i in range(1,pages):
-            if i == 1:
-                url = link
-            else:
-                url = link.split('&page')[0] + f'&page={i}'
-            driver.get(url)
-            parse_profiles_page(driver)
-        uniquedesignations = list(set(uniquedesignations))
-        with open('linkedindesignations.txt','w') as ld:
-            for u in uniquedesignations:
+                    alltext = driver.find_element(By.CSS_SELECTOR,'.pv-profile-section__section-info.section-info').text
+                    match = re.findall(r'[\w.+-]+@[\w-]+\.[\w.-]+', alltext)
+                    try:
+                        item['Email'] = match[0]
+                    except:
+                        pass
+                    driver.close()
+                    time.sleep(.5)
+                    driver.switch_to.window(driver.window_handles[0])
+                    csvwriter.writerow(item)
+                    csvoutfile.flush()
+
+            df.loc[line_count, 'LastPageScraped'] = i+1
+            time.sleep(1)
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(1)
+            while True:
                 try:
-                    ld.write(u.encode('utf-8')+'\n')
+                    driver.find_element(By.CSS_SELECTOR,'button[aria-label="Next"]').click()
+                    break
                 except:
-                    pass
+                    time.sleep(1)
+                    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
+    df.to_csv("linkedininput.csv", index=False)
     driver.quit()
-    # with open('linkedindesignations.txt','r') as ldr:
-    #     filedesignations = ldr.read().split('\n')
-    # getgooglelinks(filedesignations)
